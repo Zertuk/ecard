5a66506229a291bd0abe442eb8de005e6b870fe7
Justin <zertukis@gmail.com>
2014-04-24 12:40:25 -0500

    player can add slave card and play button is working

--- main.js
+++ main.js
@@ -1,43 +1,130 @@
+window.onload = function() {
+
 var playerTotal = 0;
 var computerTotal = 0;
+var slave = true;
+var slaveCard = document.getElementById('slave');
+var citizenCards = document.getElementsByClassName('citizen');
+var play = document.getElementById('play');
 
-// var makeHand = function() {
-// 	if (slave == false) {
-// 		var deck = new Array('Emperor', 'Citizen', 'Citizen', 'Citizen', 'Citizen');
-// 	}
-// 	else {
-// 		var deck = new Array('Slave', 'Citizen', 'Citizen', 'Citizen', 'Citizen');
-// 	}
-// 	return deck;
+var whoWins = function() {
+	// var playerDeck = makeHand();
+	// var pick1 = playerDeck[0];
+	console.log(pick1);
+	var pick2 = computerPickCard();
+	console.log(pick2);
+	var winner;
+	switch (pick1) {
+		case 'emperor': 
+			if (pick2 == 'slave') {
+				winner = 2;
+				computerTotal++;
+				break;
+			}
+			winner = 1;
+			playerTotal++;
+			break;
+						
+
+		case 'slave': 
+			if (pick2 == 'emperor') {
+				winner = 1;
+				playerTotal++;
+				break;
+			}
+			winner = 2;
+			computerTotal++;
+			break;		
+		case 'citizen':
+			if (pick2 == 'emperor') {
+				winner = 2;
+				computerTotal++;
+				break;
+			}
+			winner = 1;
+			playerTotal++;
+			break;			
+		}
+	return winner;
+}
+
+
+play.addEventListener('click', function() {
+	console.log(whoWins());
+})
+
+slaveCard.addEventListener('click', function() {
+	pick1 = 'slave';
+})
+
+citizenCards.addEventListener('click', function() {
+	var pick1 = 'citizen';
+})
+
+
+var makeHand = function() {
+ 	if (slave == false) {
+ 		var deck = new Array('emperor', 'citizen', 'citizen', 'citizen', 'citizen');
+ 	}
+ 	else {
+ 		var deck = new Array('slave', 'citizen', 'citizen', 'citizen', 'citizen');
+ 	}
+ 	return deck;
+}
+}
+
+// var pickCard = function() {
+// 	var newDeck = makeHand();
+// 	console.log(newDeck);
 // }
 
-// var firstRound = function() {
-// 	slave == false;
+// pickCard();
+
+// console.log(Math.round(Math.random()*4));
+
+//var firstRound = function() {
+//	slave == false;
 // 	player.makeHand();
-// 	slave == true;
+//  slave == true;
 // 	computer.makeHand();
-
-// }
+//}
 
 // var pickCard = function() {
 
 // }'
-var playNow = document.getElementById('play');
 
-playNow.addEventListener('click', function() {
-	whoWins();
-})
+
+
+
 
 var computerPickCard = function() {
 	var pick = Math.round(Math.random()*4);
- 	if (pick <4) {
- 		return 'Citizen';
+ 	if (pick < 4) {
+ 		return 'citizen';
  	}
  	else {
- 		return 'Emperor';
+ 		return 'emperor';
  	}
 }
 
+
+// var cards = get.ElementByTagName('cards');
+// cards.addEventListener('click', function() {
+// 	var emperor = document.getElementById('emperor');
+// 	var citizen = document.getElementById('citizen');
+// 	var slave = document.getElementById('slave');
+// 	if (slave == true) {
+// 		playerCard = 'slave';
+// 	}
+// 	else if (citizen == true) {
+// 		playerCard = 'citizen';
+// 	}
+// 	else {
+// 		playerCard = 'emperor'
+// 	}
+// }
+
+// }
 // var round = function() {
 
 // }
@@ -49,43 +136,3 @@ var computerPickCard = function() {
 
 
 
-var whoWins = function() {
-	var pick1 = 'Slave';
-	var pick2 = computerPickCard();
-	console.log(pick2);
-	var winner;
-	switch (pick1) {
-		case 'Emperor': 
-			if (pick2 == 'Slave') {
-				winner = 2;
-				computerTotal++;
-				break;
-			}
-			winner = 1;
-			PlayerTotal++;
-			break;
-						
-
-		case 'Slave': 
-			if (pick2 == 'Emperor') {
-				winner = 1;
-				PlayerTotal++;
-				break;
-			}
-			winner = 2;
-			computerTotal++;
-			break;		
-		case 'Citizen':
-			if (pick2 == 'Emperor') {
-				winner = 2;
-				computerTotal++;
-				break;
-			}
-			winner = 1;
-			PlayerTotal++;
-			break;			
-		}
-	return winner;
-}
-
-console.log(whoWins());
